f68aacd9bc2c357bca9f3e54fcb1ccf5d35ac636
jawad0098 <hassanjawad858@gmail.com>
2023-08-29 01:58:24 -0700

    assignment7

---
+++ app.js
@@ -0,0 +1,70 @@
+chapter =1
+
+
+// alert("Error! Please enter a valid password.")
+
+// alert("Welcome to JS land... \n Happy Coding!")
+
+// alert("Welcome to JS land...");
+
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// alert("Hello...I can run JS through my web browser's console");
+
+
+
+chapter =2
+
+// var username="jawad";
+
+// console.log(username);
+
+// var myName="Muhammad Jawad Ul Hassan"
+
+// console.log(myName)
+
+// var message="Hello World"
+
+// console.log(message)
+
+// alert("Hello world")
+
+// JS variables
+
+
+// var studentName = "Jawad";
+
+// var age = 25;
+
+// var course = "SMIT";
+
+// var country = "Pakistan";
+
+// // alert boxes
+
+
+// alert("Student Name: " + studentName);
+
+// alert("Age: " + age);
+
+// alert("Course: " + course);
+
+// alert("Country: " + country);
+
+
+// alert("PIZZA\n PIZZ\n PIZ\n PI\n P")
+
+// var email="hassanjawad858@gamil.com";
+// alert("My email adress is:"+email);
+
+// var book="A smarter way to learn JavaScript";
+// alert("i am trying to learn from the book "+ book);
+
+
+// var text = "Yah! I can write HTML content through JavaScript ";
+//         document.write(text);
+
+
+// var myString = "=======This Is String========";
+// alert(myString);
+// document.write(myString);
